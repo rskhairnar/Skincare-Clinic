34c289b9522069cd177521c5eaabe47c4a0d67c0
rskhairnar <rskhairnar212@gmail.com>
2026-03-19 12:36:19 +0530

    Simple fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,17 +3,6 @@ const nextConfig = {
   images: { 
     unoptimized: true 
   },
-  output: 'standalone',
-  
-  // Disable all static page generation
-  experimental: {
-    isrFlushToDisk: false,
-  },
-  
-  // Force all pages to be server-rendered
-  async headers() {
-    return [];
-  },
 };
 
 export default nextConfig;
